b4f04af0d3f63737bddcb6e7b47c59532fa1f113
Latitude 7420 <amorusnn@gmail.ru>
2023-06-19 15:05:17 +0600

    Set up the bottom navigation

--- app/src/main/java/com/example/myinstaclone/MainActivity.kt
+++ app/src/main/java/com/example/myinstaclone/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.myinstaclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.myinstaclone.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        val botNav: BottomNavigationView = binding.mainBottomNavView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+        botNav.setupWithNavController(navController)
     }
-
 }
